d717251dfa2e64948abe86881d0ea1f21fe1888d
Elie Zananiri <prisonerjohn@gmail.com>
2014-09-16 16:25:47 -0400

    Updated example.

--- example/src/ofApp.cpp
+++ example/src/ofApp.cpp
@@ -1,84 +1,105 @@
+//
+//  ofApp.cpp
+//  ofxAutomatedInputExample
+//
+//  Created by Elie Zananiri on 2014-09-16.
+//
+//
+
 #include "ofApp.h"
 
 //--------------------------------------------------------------
-void ofApp::setup(){
-    ofAddListener( autoInput.AUTOMATED_INPUT , this , &ofApp::automatedInputHandler ) ;
-}
-
-//--------------------------------------------------------------
-void ofApp::update(){
-    autoInput.update() ;
-}
-
-//--------------------------------------------------------------
-void ofApp::draw(){
-
-    autoInput.draw( ) ;
-    stringstream ss ;
-    string boolString ;
-    if ( autoInput.getIsRecording() == true )
-        boolString = " true " ;
-    else
-        boolString = " false " ;
-
-    string playingString ;
-    if ( autoInput.getIsPlaying() == true )
-        playingString = " true " ;
-    else
-        playingString = " false " ;
-
-    ss << " (T) - Is it recording ? " << boolString << endl ;
-    ss << " (P) - Is it playing ? " << playingString << endl ;
-    ss << " (R) - reset data" << endl ;
-    ss << " (S) - save data to XML" << endl ;
-    ss << " (L) - load data from XML" << endl ;
-    ss << autoInput.getDebugString() << endl ;
-
-    ofDrawBitmapStringHighlight( ss.str() , 15 , 25 ) ;
-}
-
-void ofApp::automatedInputHandler( AutomatedInputData &args )
+void ofApp::setup()
 {
-    cout << " EVENT IS INPUTING ! " << endl ;
-    float hue = ( args.x / ofGetWidth() ) * 255.0f ;
-    float saturation = ( args.y / ofGetHeight() ) * 255.0f ;
-    ofColor bg_color = ofColor::fromHsb( hue , saturation , 185.0f ) ;
-    ofBackground( bg_color ) ;
+    ofSetLogLevel(OF_LOG_NOTICE);
+    ofSetFrameRate(60);
+    ofSetVerticalSync(true);
+    ofSetBackgroundAuto(false);
+    ofBackground(0);
+    
+    ofAddListener(automatedInput.playbackFinishedEvent, this, &ofApp::automatedInputPlaybackFinished);
 }
+
 //--------------------------------------------------------------
-void ofApp::keyPressed(int key){
+void ofApp::update()
+{
 
-    ofFileDialogResult result ;
-    switch ( key )
-    {
-        case 't' :
-        case 'T':
-            autoInput.toggleRecording() ;
-            break ;
+}
 
-        case 'r' :
+//--------------------------------------------------------------
+void ofApp::draw()
+{
+//
+//    autoInput.draw( ) ;
+//    string boolString ;
+//    if ( autoInput.getIsRecording() == true )
+//        boolString = " true " ;
+//    else
+//        boolString = " false " ;
+//
+//    string playingString ;
+//    if ( autoInput.getIsPlaying() == true )
+//        playingString = " true " ;
+//    else
+//        playingString = " false " ;
+//
+
+    stringstream ss;
+    ss << "ofxAutomatedInput" << endl;
+    ss << " [R]ecording? " << (automatedInput.isRecording()? "YES":"NO") << endl;
+    ss << " [P]laying? " << (automatedInput.isPlaying()? "YES":"NO") << endl;
+    ss << " [C]lear data" << endl;
+    ss << " [S]ave data to XML" << endl;
+    ss << " [L]oad data from XML" << endl;
+    ofDrawBitmapStringHighlight(ss.str(), 15, 25);
+}
+
+//void ofApp::automatedInputHandler( AutomatedInputData &args )
+//{
+//    cout << " EVENT IS INPUTING ! " << endl ;
+//    float hue = ( args.x / ofGetWidth() ) * 255.0f ;
+//    float saturation = ( args.y / ofGetHeight() ) * 255.0f ;
+//    ofColor bg_color = ofColor::fromHsb( hue , saturation , 185.0f ) ;
+//    ofBackground( bg_color ) ;
+//}
+
+//--------------------------------------------------------------
+void ofApp::keyPressed(int key)
+{
+    ofFileDialogResult result;
+    switch (key) {
+        case 'r':
         case 'R':
-            autoInput.reset() ;
+            ofBackground(0);
+            automatedInput.toggleRecording();
+            break;
+            
+        case 'p':
+        case 'P':
+            ofBackground(0);
+            automatedInput.startPlayback();
+            break;
+            
+        case 'c':
+        case 'C':
+            automatedInput.clear();
             break ;
 
         case 's' :
         case 'S':
-            result = ofSystemSaveDialog( "data.xml" , "Export Input Data as XML" ) ;
-            if ( result.bSuccess )
-                autoInput.saveXml( result.getPath() ) ;
-            break ;
+            result = ofSystemSaveDialog("data.xml", "Export input data to XML" );
+            if (result.bSuccess) {
+                automatedInput.saveToXml(result.getPath());
+            }
+            break;
 
         case 'l':
         case 'L':
-            result = ofSystemLoadDialog( "Select an input data XML" , false ) ;
-            if ( result.bSuccess )
-                autoInput.loadXml( result.getPath() ) ;
-            break ;
-
-        case 'p':
-        case 'P':
-            autoInput.togglePlayback() ;
-            break ;
+            result = ofSystemLoadDialog("Select an input data XML file", false);
+            if (result.bSuccess) {
+                automatedInput.loadFromXml(result.getPath());
+            }
+            break;
     }
 }
 
@@ -93,31 +114,26 @@ void ofApp::mouseMoved(int x, int y){
 }
 
 //--------------------------------------------------------------
-void ofApp::mouseDragged(int x, int y, int button){
+void ofApp::mouseDragged(int x, int y, int button)
+{
+    ofSetColor(255);
+    ofCircle(x, y, 25);
+}
+
+//--------------------------------------------------------------
+void ofApp::mousePressed(int x, int y, int button)
+{
 
 }
 
 //--------------------------------------------------------------
-void ofApp::mousePressed(int x, int y, int button){
+void ofApp::mouseReleased(int x, int y, int button)
+{
 
 }
 
 //--------------------------------------------------------------
-void ofApp::mouseReleased(int x, int y, int button){
-    autoInput.addInput( x , y , 0 ) ;
-}
-
-//--------------------------------------------------------------
-void ofApp::windowResized(int w, int h){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::gotMessage(ofMessage msg){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::dragEvent(ofDragInfo dragInfo){
-
+void ofApp::automatedInputPlaybackFinished(unsigned long long& duration)
+{
+    ofBackground(0);
 }
--- example/src/ofApp.h
+++ example/src/ofApp.h
@@ -1,34 +1,36 @@
+//
+//  ofApp.h
+//  ofxAutomatedInputExample
+//
+//  Created by Elie Zananiri on 2014-09-16.
+//
+//
+
 #pragma once
 
-/*
-    Created by Ben McChesney
-    @ Helios Interactive
-
-    301 8th Street, STE 250
-    San Francisco , CA
-
-    5/3/2013
-*/
 #include "ofMain.h"
 #include "ofxAutomatedInput.h"
 
-class ofApp : public ofBaseApp{
-	public:
-		void setup();
-		void update();
-		void draw();
-
-		void keyPressed(int key);
-		void keyReleased(int key);
-		void mouseMoved(int x, int y);
-		void mouseDragged(int x, int y, int button);
-		void mousePressed(int x, int y, int button);
-		void mouseReleased(int x, int y, int button);
-		void windowResized(int w, int h);
-		void dragEvent(ofDragInfo dragInfo);
-		void gotMessage(ofMessage msg);
-
-		ofxAutomatedInput autoInput ;
-
-		void automatedInputHandler( AutomatedInputData &args ) ;
+//--------------------------------------------------------------
+class ofApp
+: public ofBaseApp
+{
+public:
+    void setup();
+    void update();
+    void draw();
+    
+    void keyPressed(int key);
+    void keyReleased(int key);
+    void mouseMoved(int x, int y);
+    void mouseDragged(int x, int y, int button);
+    void mousePressed(int x, int y, int button);
+    void mouseReleased(int x, int y, int button);
+//    void windowResized(int w, int h);
+//    void dragEvent(ofDragInfo dragInfo);
+//    void gotMessage(ofMessage msg);
+    
+    void automatedInputPlaybackFinished(unsigned long long& duration);
+    
+    ofxAutomatedInput automatedInput;
 };
